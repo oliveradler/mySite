2691aca456c8c431b6185281b21ec7aade9cc4c8
oliveradler <oliver.adler2145@gmail.com>
2024-09-19 15:25:40 +0200

    Initial commit, home page with links, header & footer

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles.css">
+</head>
+
+<body>
+    <div id="header">
+        <div class="navigation_item">
+            <a href="./index.php">Home</a>
+        </div>
+        <div class="navigation_item">
+            <a href="./projects.php">Projects</a>
+        </div>
+    </div>
+    <div id="main">
+        <p>Some Text</p>
+    </div>
+    <div id="footer">
+        <div class="navigation_item"> <a href="https://www.linkedin.com/in/oliver-adler-6b4447322/">LinkedIn</a></div>
+        <div class="navigation_item"> <a href="https://github.com/oliveradler">Git</a></div>
+    </div>
+</body>
+
+</html>
